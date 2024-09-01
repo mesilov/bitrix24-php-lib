c28b8bdd9e3ca7d737cb34cafede784e6170958f
mesilov <mesilov.maxim@gmail.com>
2024-09-01 19:32:15 +0600

    Add Bitrix24Account entity and unit tests
    This commit introduces the `Bitrix24Account` entity with full implementation for managing Bitrix24 account statuses, auth tokens, and domain URLs. It also includes unit tests for the added entity, and updates the `composer.json` to reflect changes in namespace configurations.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

---
+++ tests/Unit/Bitrix24Accounts/Entity/Bitrix24AccountTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-app-core package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\ApplicationCore\Tests\Unit\Bitrix24Accounts\Entity;
+
+use Bitrix24\SDK\Application\Contracts\Bitrix24Accounts\Entity\Bitrix24AccountInterface;
+use Bitrix24\SDK\Application\Contracts\Bitrix24Accounts\Entity\Bitrix24AccountStatus;
+use Bitrix24\SDK\ApplicationCore\Bitrix24Accounts\Entity\Bitrix24Account;
+use Bitrix24\SDK\Core\Credentials\AuthToken;
+use Bitrix24\SDK\Core\Credentials\Scope;
+use Bitrix24\SDK\Tests\Application\Contracts\Bitrix24Accounts\Entity\Bitrix24AccountInterfaceTest;
+use Carbon\CarbonImmutable;
+use Override;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Component\Uid\Uuid;
+
+#[CoversClass(Bitrix24Account::class)]
+class Bitrix24AccountTest extends Bitrix24AccountInterfaceTest
+{
+    #[Override]
+    protected function createBitrix24AccountImplementation(
+        Uuid                  $uuid,
+        int                   $bitrix24UserId,
+        bool                  $isBitrix24UserAdmin,
+        string                $memberId,
+        string                $domainUrl,
+        Bitrix24AccountStatus $bitrix24AccountStatus,
+        AuthToken             $authToken,
+        CarbonImmutable       $createdAt,
+        CarbonImmutable       $updatedAt,
+        int                   $applicationVersion,
+        Scope                 $applicationScope
+    ): Bitrix24AccountInterface
+    {
+        return new Bitrix24Account(
+            $uuid,
+            $bitrix24UserId,
+            $isBitrix24UserAdmin,
+            $memberId,
+            $domainUrl,
+            $bitrix24AccountStatus,
+            $authToken,
+            $createdAt,
+            $updatedAt,
+            $applicationVersion,
+            $applicationScope
+        );
+    }
+}
