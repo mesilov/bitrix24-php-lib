0ec7bbc673b1323cd76b16e4653d807067638d63
mesilov <mesilov.maxim@gmail.com>
2024-10-28 00:06:36 +0600

    Enhance PHP-CS-Fixer configuration with additional rules
    Updated .php-cs-fixer.dist.php to include cache file and new rule sets (@Symfony, @DoctrineAnnotation, @PhpCsFixer). Also adjusted the string concatenation formatting for better readability.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,8 +2,14 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/src');
+    ->in(__DIR__ . '/src');
 
 return (new PhpCsFixer\Config())
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
-    ->setFinder($finder);
+    ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
+    ->setFinder($finder)
+    ->setRules([
+        '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
+        '@PhpCsFixer' => true,
+    ]);
