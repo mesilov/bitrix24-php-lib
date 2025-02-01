c7a4d8b6b27f31d6df7fb678e2c58bb7c9857eca
KarlsonComplete <k_hram@mail.ru>
2025-02-01 14:07:36 +0300

    - Правки для сs fixer

--- src/Bitrix24Accounts/UseCase/ChangeDomainUrl/Command.php
+++ src/Bitrix24Accounts/UseCase/ChangeDomainUrl/Command.php
@@ -23,13 +23,13 @@ readonly class Command
     private function validateDomain(string $domain, string $parameterName): void
     {
         // Регулярное выражение для проверки допустимых символов (латиница и кириллица)
-        $patternValidChars = '/^((?!-)[A-Za-zА-Яа-яЁё0-9-]{1,63}(?<!-)\\.)+[A-Za-zА-Яа-яЁё]{2,6}$/u';
+        $patternValidChars = '/^((?!-)[A-Za-zА-Яа-яЁё0-9-]{1,63}(?<!-)\.)+[A-Za-zА-Яа-яЁё]{2,6}$/u';
 
         // Проверка общей длины (1-253 символа)
         $patternLengthCheck = '/^.{1,253}$/';
 
         // Проверка длины каждой метки (1-63 символа, включая кириллицу)
-        $patternLengthEachLabel = '/^[A-Za-zА-Яа-яЁё0-9-]{1,63}(\\.[A-Za-zА-Яа-яЁё0-9-]{1,63})*$/u';
+        $patternLengthEachLabel = '/^[A-Za-zА-Яа-яЁё0-9-]{1,63}(\.[A-Za-zА-Яа-яЁё0-9-]{1,63})*$/u';
         if (
             in_array(preg_match($patternValidChars, $domain), [0, false], true)
             || in_array(preg_match($patternLengthCheck, $domain), [0, false], true)
