0fe2002ce997dd604e02aad5b04d5f943d462672
KarlsonComplete <k_hram@mail.ru>
2025-07-13 13:49:19 +0300

    Дописал юнит тесты для установщика.

--- tests/Unit/ApplicationInstallations/UseCase/Install/CommandTest.php
+++ tests/Unit/ApplicationInstallations/UseCase/Install/CommandTest.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 
 namespace Bitrix24\Lib\Tests\Unit\ApplicationInstallations\UseCase\Install;
 
-use Bitrix24\Lib\Bitrix24Accounts\UseCase\ChangeDomainUrl\Command;
 use Bitrix24\Lib\Bitrix24Accounts\ValueObjects\Domain;
 use Bitrix24\Lib\Tests\Functional\Bitrix24Accounts\Builders\Bitrix24AccountBuilder;
 use Bitrix24\SDK\Application\ApplicationStatus;
 use Bitrix24\SDK\Application\PortalLicenseFamily;
 use Bitrix24\SDK\Core\Credentials\AuthToken;
 use Bitrix24\SDK\Core\Credentials\Scope;
+use Bitrix24\SDK\Core\Exceptions\InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
-use Bitrix24\Lib\ApplicationInstallations\UseCase\Install;
+use Bitrix24\Lib\ApplicationInstallations\UseCase\Install\Command;
 use Symfony\Component\Uid\Uuid;
 use Bitrix24\Lib\Tests\Functional\ApplicationInstallations\Builders\ApplicationInstallationBuilder;
 
@@ -53,7 +53,7 @@ class CommandTest extends TestCase
             $this->expectException($expectedException);
         }
 
-        new Install\Command(
+        $command = new Command(
             $applicationStatus,
             $portalLicenseFamily,
             $portalUsersCount,
@@ -71,6 +71,11 @@ class CommandTest extends TestCase
             $applicationScope,
             $applicationToken,
         );
+
+        if (null === $expectedException) {
+            // Проверяем, что объект создан и является экземпляром нужного класса
+            $this->assertInstanceOf(Command::class, $command);
+        }
     }
 
     public static function dataForCommand(): \Generator
@@ -84,8 +89,7 @@ class CommandTest extends TestCase
             ->withApplicationScope(new Scope(['crm']))
             ->build();
 
-
-        yield '' => [
+        yield 'validCommand' => [
             $applicationInstallationBuilder->getApplicationStatus(),
             $applicationInstallationBuilder->getPortalLicenseFamily(),
             $applicationInstallationBuilder->getPortalUsersCount(),
@@ -94,14 +98,158 @@ class CommandTest extends TestCase
             $applicationInstallationBuilder->getBitrix24PartnerId(),
             $applicationInstallationBuilder->getExternalId(),
             $applicationInstallationBuilder->getComment(),
-            $applicationInstallationBuilder->getBitrix24AccountId(),
             $bitrix24AccountBuilder->getBitrix24UserId(),
             $bitrix24AccountBuilder->isBitrix24UserAdmin(),
             $bitrix24AccountBuilder->getMemberId(),
-            $bitrix24AccountBuilder->getDomainUrl(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
             $bitrix24AccountBuilder->getAuthToken(),
             $bitrix24AccountBuilder->getApplicationVersion(),
             $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            null,
+        ];
+
+        yield 'invalidPortalUsersCount' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            0,
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            $applicationInstallationBuilder->getComment(),
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+        yield 'emptyExternalId' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            '',
+            $applicationInstallationBuilder->getComment(),
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+        yield 'emptyComment' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            '',
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+
+        yield 'invalidBitrix24UserId' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            $applicationInstallationBuilder->getComment(),
+            0,
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+        // Пустой memberId
+        yield 'emptyMemberId' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            $applicationInstallationBuilder->getComment(),
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            '',
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+        // Некорректная версия приложения
+        yield 'invalidApplicationVersion' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            $applicationInstallationBuilder->getComment(),
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            0,
+            $bitrix24AccountBuilder->getApplicationScope(),
+            null,
+            InvalidArgumentException::class,
+        ];
+
+        yield 'emptyApplicationToken' => [
+            $applicationInstallationBuilder->getApplicationStatus(),
+            $applicationInstallationBuilder->getPortalLicenseFamily(),
+            $applicationInstallationBuilder->getPortalUsersCount(),
+            $applicationInstallationBuilder->getContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerContactPersonId(),
+            $applicationInstallationBuilder->getBitrix24PartnerId(),
+            $applicationInstallationBuilder->getExternalId(),
+            $applicationInstallationBuilder->getComment(),
+            $bitrix24AccountBuilder->getBitrix24UserId(),
+            $bitrix24AccountBuilder->isBitrix24UserAdmin(),
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $bitrix24AccountBuilder->getAuthToken(),
+            $bitrix24AccountBuilder->getApplicationVersion(),
+            $bitrix24AccountBuilder->getApplicationScope(),
+            '',
+            InvalidArgumentException::class
         ];
 
     }
--- tests/Unit/ApplicationInstallations/UseCase/OnAppInstall/CommandTest.php
+++ tests/Unit/ApplicationInstallations/UseCase/OnAppInstall/CommandTest.php
@@ -1,15 +1,23 @@
 <?php
 
-
 declare(strict_types=1);
 
 namespace Bitrix24\Lib\Tests\Unit\ApplicationInstallations\UseCase\OnAppInstall;
 
-use Bitrix24\Lib\Bitrix24Accounts\UseCase\ChangeDomainUrl\Command;
+use Bitrix24\Lib\ApplicationInstallations\UseCase\OnAppInstall\Command;
+use Bitrix24\Lib\Bitrix24Accounts\ValueObjects\Domain;
+use Bitrix24\Lib\Tests\Functional\ApplicationInstallations\Builders\ApplicationInstallationBuilder;
+use Bitrix24\Lib\Tests\Functional\Bitrix24Accounts\Builders\Bitrix24AccountBuilder;
+use Bitrix24\SDK\Application\ApplicationStatus;
+use Bitrix24\SDK\Application\Contracts\ApplicationInstallations\Entity\ApplicationInstallationStatus;
+use Bitrix24\SDK\Application\Contracts\Bitrix24Accounts\Entity\Bitrix24AccountStatus;
+use Bitrix24\SDK\Application\PortalLicenseFamily;
+use Bitrix24\SDK\Core\Credentials\Scope;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @internal
@@ -17,5 +25,85 @@ use PHPUnit\Framework\Attributes\Test;
 #[CoversClass(Command::class)]
 class CommandTest extends TestCase
 {
+    #[Test]
+    #[DataProvider('dataForCommand')]
+    public function testValidCommand(
+        string  $memberId,
+        Domain  $domain,
+        string  $applicationToken,
+        string  $applicationStatus,
+        ?string $expectedException,
+    ): void
+    {
+        if (null !== $expectedException) {
+            $this->expectException($expectedException);
+        }
 
+        $command = new Command(
+            $memberId,
+            $domain,
+            $applicationToken,
+            $applicationStatus
+        );
+
+        if (null === $expectedException) {
+            $this->assertInstanceOf(Command::class, $command);
+        }
+    }
+
+    public static function dataForCommand(): \Generator
+    {
+        $applicationToken = Uuid::v7()->toRfc4122();
+        $applicationStatus = 'T';
+
+        $applicationInstallationBuilder = (new ApplicationInstallationBuilder())
+            ->withApplicationStatus(new ApplicationStatus('F'))
+            ->withPortalLicenseFamily(PortalLicenseFamily::free)
+            ->withApplicationToken($applicationToken)
+            ->withApplicationStatusInstallation(ApplicationInstallationStatus::active)
+            ->build();
+
+        $bitrix24AccountBuilder = (new Bitrix24AccountBuilder())
+            ->withApplicationScope(new Scope(['crm']))
+            ->withInstalled()
+            ->withApplicationToken($applicationToken)
+            ->withStatus(Bitrix24AccountStatus::active)
+            ->build();
+
+        // Валидный кейс
+        yield 'validCommand' => [
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $applicationToken,
+            $applicationStatus,
+            null,
+        ];
+
+        // Пустой memberId
+        yield 'emptyMemberId' => [
+            '',
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $applicationToken,
+            $applicationStatus,
+            \InvalidArgumentException::class,
+        ];
+
+        // Пустой applicationToken
+        yield 'emptyApplicationToken' => [
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            '',
+            $applicationStatus,
+            \InvalidArgumentException::class,
+        ];
+
+        // Пустой applicationStatus
+        yield 'emptyApplicationStatus' => [
+            $bitrix24AccountBuilder->getMemberId(),
+            new Domain($bitrix24AccountBuilder->getDomainUrl()),
+            $applicationToken,
+            '',
+            \InvalidArgumentException::class,
+        ];
+    }
 }
